a77f8c39666f22cf4a7bc5d8b5888b0bffd4e719
Trần Trung Hiếu <trunghieu05092001@gmail.com>
2022-04-17 22:09:40 +0700

    update index.js in server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,53 @@
-var express = require('express')
-var cors = require('cors')
-var app = express()
+var express = require("express");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+var mysql = require("mysql");
 
-app.use(cors())
+var app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true 
+}));
+
+var con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'shoe_shop_db'
+})
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 
 app.get('/', (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello!");
 })
+
+con.connect();
+
+// Truy xuất tất cả dữ liệu user
+app.get('/users', function (req, res) {
+    dbConn.query('SELECT * FROM users', function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'users list.' });
+    });
+});
+
+app.get('/items', function (req, res) {
+    var sql = 'SELECT gi.id, c.fullname, gi.categoryid, gi.itemname, gi.itemtype ';
+    sql += ' FROM mdl_grade_items, mdl_course c ';
+    sql += ' WHERE gi.courseid = c.id and gi.hidden = 0';
+
+    con.query(sql, function(error, results, fields) {
+        if (error) throw error;
+        return res.send(results);
+    });
+});
 
 app.get('/cors', (req, res) => { 
     res.send('This has CORS enabled'); 
@@ -18,4 +59,7 @@ app.get('/products/:id', cors(), function (req, res, next) {
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, console.log(`CORS-enabled web server listening on port ${PORT}`));
+app.listen(PORT, function() {
+    console.log(`CORS-enabled web server listening on port ${PORT}`);
+});
+module.exports = app;
