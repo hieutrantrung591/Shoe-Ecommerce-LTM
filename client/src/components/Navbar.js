cddc15553954698d709aaf99136b8481a63e2d01
Trần Trung Hiếu <trunghieu05092001@gmail.com>
2022-04-24 17:01:12 +0700

    Update Navbar in Client

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import logo from '../assets/image/logo/logo-small.png';
+import './Navbar.css';
 
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="# ">
-          <img src={logo} alt="Poseidon Logo" draggable="false" height="80"/>
-        </a>
+      <div className="container">
+        <Link to="/" className="navbar-brand">
+          <img src={logo} alt="Poseidon Logo" draggable="false" height="50"/>  
+        </Link>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -16,28 +17,33 @@ function Navbar() {
 
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav me-3">
-            <li className="nav-item p-2">
+            <li className="nav-item">
               <Link to="/">Men</Link>  
             </li>
-            <li className="nav-item p-2">
+            <li className="nav-item">
               <Link to="/about">Women</Link>
             </li>
-            <li className="nav-item p-2">
+            <li className="nav-item">
               <Link to="/contact">Special</Link>
             </li>
-            <li className="nav-item p-2">
+            <li className="nav-item">
               <Link to="/projects">Sale</Link>
             </li>
           </ul>
 
           <form className="d-flex align-items-center w-100 form-search">
             <div className="input-group">
+              <div className="input-group-prepend">
+                <span className="input-group-text">
+                  <i className="bi bi-search"></i>
+                </span>
+              </div>
               <input type="search" className="form-control" placeholder="Search" aria-label="Search" />
             </div>
             <a href="#!" className="text-white"><i className="fas fa-search ps-3"></i></a>
           </form>
 
-          <button type="button" className="btn">
+          <button type="button" className="btn me-2">
             <i class="bi bi-heart"></i>
           </button>
           <button type="button" className="btn">
